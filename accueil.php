cba05319a617ca92056a3dda5604c970956aa07a
Louaiy LETIFI <lleti@lmdsio.com>
2026-09-03 14:38:21 +0200

    Mon projet version 2

--- accueil.php
+++ accueil.php
@@ -1,5 +1,11 @@
 <?php
-$prenom = $_POST['prenom'];
-$nom = $_POST['nom'];
-echo "Bonjour $prenom $nom";
+$login = $_POST['login'];
+$mdp = $_POST['mdp'];
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+if ($login == "admin" && $mdp == "azerty") {
+    header("Location: http://$host$uri/profil.html");
+} else {
+    header("Location: http://$host$uri/index.html");
+}
 ?>
