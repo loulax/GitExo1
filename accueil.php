2fa1e88e33e2473aef1aad177f28cfb602ff0232
Louaiy LETIFI <lleti@lmdsio.com>
2026-09-03 14:44:36 +0200

    Mon projet version 3

--- accueil.php
+++ accueil.php
@@ -4,8 +4,8 @@ $mdp = $_POST['mdp'];
 $host = $_SERVER['HTTP_HOST'];
 $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 if ($login == "admin" && $mdp == "azerty") {
-    header("Location: http://$host$uri/profil.html");
-} else {
     header("Location: http://$host$uri/index.html");
+} else {
+    header("Location: http://$host$uri/profil.html");
 }
 ?>
